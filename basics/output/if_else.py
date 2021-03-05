90a179a1f64cf8ffc2b040e464a74d59a93a0663
wattuhewa <sdeshan@yahoo.com>
2021-03-05 09:16:03 +0000

    Added code to demonstrate the use of an if else statement

---
+++ basics/output/if_else.py
@@ -0,0 +1,10 @@
+# check whether first number is bigger than the second number.
+# Assume first_number and second_number are existing variables.
+first_number = 50
+second_number =20
+if (first_number > second_number):
+    print("First is bigger!")
+else:
+    print("First is equal or smaller!")
+
+print("Done!")
