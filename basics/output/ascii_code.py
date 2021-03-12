81ba3fac9d29a6eeb8d8ac004de7460537169486
wattuhewa <sdeshan@yahoo.com>
2021-03-12 09:48:27 +0000

    User Define Function example

--- basics/output/ascii_code.py
+++ basics/output/ascii_code.py
@@ -1,11 +1,2 @@
-print("Program Started!")
-
-print("Please enter a standard character:")
-character = input()
-
-if (len(character) == 1):
-    print("The ASCII code for {} is {}".format(character, ord(character))
-else:
-    print("A single character was expected.")
-
-print("Program Ended!")
+c = 'p'
+print("The ASCII value of '" + c + "' is", ord(c))
---
+++ basics/output/user_defined_functions.py
@@ -0,0 +1,12 @@
+
+#The Function
+
+def greet_user():
+    print("Please enter your name")
+    name=input()
+    print("Hello ",name)
+
+# Call to function
+greet_user()
+greet_user()
+greet_user()
