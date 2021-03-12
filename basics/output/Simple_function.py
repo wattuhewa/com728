62b3014d0bd26b4dd76aa96f5d26e108b72e8a48
wattuhewa <sdeshan@yahoo.com>
2021-03-12 10:06:11 +0000

    Added code to demonstrate the use of a user-defined function

---
+++ basics/output/Simple_function.py
@@ -0,0 +1,14 @@
+#what sound did I here?
+#rumble
+#That was a loud rumble!
+
+def listen():
+    #Ask user for the sound
+    print("what sound did I hear?")
+    sound = input()
+
+    #Display the message
+    print("\n That was a loud", sound)
+
+#call to function
+listen()
--- basics/output/user_defined_functions.py
+++ basics/output/user_defined_functions.py
@@ -1,12 +1,1 @@
 
-#The Function
-
-def greet_user():
-    print("Please enter your name")
-    name=input()
-    print("Hello ",name)
-
-# Call to function
-greet_user()
-greet_user()
-greet_user()
